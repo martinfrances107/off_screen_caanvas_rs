7162f106a3aa3fad86827d8369da803b134d5c5f
Martin <martinfrances107@hotmail.com>
2023-05-13 13:07:51 +0100

    Now using web_sys_unstable_apis. Now using Path2d. This is the first working verison.

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,69 @@
-extern crate js_sys;
-extern crate web_sys;
-
 use std::f64;
 use wasm_bindgen::prelude::*;
-use web_sys::HtmlCanvasElement;
-use web_sys::ImageBitmapRenderingContext;
-use web_sys::OffScreenCanvasRenderingContext2d;
-use web_sys::OffscreenCanvas;
+
+use web_sys::Path2d;
 
 #[wasm_bindgen(start)]
-fn start() -> Result<(), JsValue> {
+fn start() {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: HtmlCanvasElement = canvas
-        .dyn_into::<HtmlCanvasElement>()
+    let onscreen_canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
 
-    let context = canvas
+    let onscreen_context = onscreen_canvas
         .get_context("bitmaprenderer")
         .unwrap()
         .unwrap()
-        .dyn_into::<ImageBitmapRenderingContext>()
+        .dyn_into::<web_sys::ImageBitmapRenderingContext>()
         .unwrap();
 
-    let off_canvas = OffscreenCanvas::new(800, 600)?;
+    let off_canvas: web_sys::OffscreenCanvas =
+        web_sys::OffscreenCanvas::new(800, 600).expect("allocation of OffscreenCanvas failed");
 
-    let off_canvas_context = off_canvas
+    let context = off_canvas
         .get_context("2d")
         .unwrap()
         .unwrap()
-        .dyn_into::<OffScreenCanvasRenderingContext2d>()
+        .dyn_into::<web_sys::OffscreenCanvasRenderingContext2d>()
         .unwrap();
-
-    off_canvas_context.begin_path();
+    context.begin_path();
 
     // Draw the outer circle.
-    off_canvas_context
+    let path_face = Path2d::new().unwrap();
+    path_face
         .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
         .unwrap();
 
     // Draw the mouth.
-    off_canvas_context.move_to(110.0, 75.0);
-    off_canvas_context
+    let path_mouth = Path2d::new().unwrap();
+    path_mouth.move_to(110.0, 75.0);
+    path_mouth
         .arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI)
         .unwrap();
 
     // Draw the left eye.
-    off_canvas_context.move_to(65.0, 65.0);
-    off_canvas_context
+    let path_l_eye = Path2d::new().unwrap();
+    path_l_eye.move_to(65.0, 65.0);
+    path_l_eye
         .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
         .unwrap();
 
     // Draw the right eye.
-    off_canvas_context.move_to(95.0, 65.0);
-    off_canvas_context
+    let path_r_eye = Path2d::new().unwrap();
+    path_r_eye.move_to(95.0, 65.0);
+    path_r_eye
         .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
         .unwrap();
 
-    off_canvas_context.stroke();
+    path_face.add_path(&path_mouth);
+    path_face.add_path(&path_l_eye);
+    path_face.add_path(&path_r_eye);
+    context.stroke_with_path(&path_face);
+    // context.stroke();
 
-    let bitmap = off_canvas.transfer_to_image_bitmap()?;
+    let bitmap = off_canvas.transfer_to_image_bitmap().unwrap();
 
-    context.transfer_from_image_bitmap(&bitmap);
-
-    Ok(())
+    onscreen_context.transfer_from_image_bitmap(&bitmap);
 }
